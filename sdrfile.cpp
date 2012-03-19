5c1734a28da89b94dcf8d7051aaa5de4ead970b0
rlandsman <rlandsman@localhost>
2012-03-19 19:30:41 +0000

    WIP: Hardwired to read test.wav in specific directory.  Works, but problems with initializing receiver mode, no ui to specify file, etc.

--- sdrfile.cpp
+++ sdrfile.cpp
@@ -1,19 +1,150 @@
 #include "sdrfile.h"
+#include "Receiver.h"
+#include "Settings.h"
+#include "Demod.h"
+
+void ReadWavTest();
 
 SDRFile::SDRFile(Receiver *_receiver,SDRDEVICE dev,Settings *_settings): SDR(_receiver, dev,_settings)
 {
-}
-SDRFile::~SDRFile(void) {}
 
-bool SDRFile::Connect() {return true;}
-bool SDRFile::Disconnect() {return true;}
+    framesPerBuffer = settings->framesPerBuffer;
+    inBuffer = CPXBuf::malloc(framesPerBuffer);
+    outBuffer = CPXBuf::malloc(framesPerBuffer);
+
+    nco = new NCO(settings->sampleRate,framesPerBuffer);
+    nco->SetFrequency(100);
+
+    wavFile = NULL;
+
+    producerThread = new SDRProducerThread(this);
+    producerThread->setRefresh(0); //Semaphores will block and wait, no extra delay needed
+    consumerThread = new SDRConsumerThread(this);
+    consumerThread->setRefresh(0);
+
+}
+SDRFile::~SDRFile(void)
+{
+}
+
+bool SDRFile::Connect()
+{
+    //WIP: Chose file or fixed internal generator
+    //test.wav for now
+    bool res = OpenWavFile("/Users/rlandsman/Documents/SDR/Pebble/PebbleII/PebbleQt-build-desktop-Desktop_Qt_4_8_0_for_GCC__Qt_SDK__Debug/test.wav");
+    if (!res)
+        return false;
+
+    return true;
+}
+bool SDRFile::Disconnect()
+{
+    if (wavFile != NULL)
+        wavFile->close();
+    return true;
+}
 double SDRFile::SetFrequency(double fRequested,double fCurrent) {return 0;}
 void SDRFile::ShowOptions() {return;}
-void SDRFile::Start() {return;}
-void SDRFile::Stop() {return;}
+
+void SDRFile::Start()
+{
+    //WIP: We don't need both threads, just the producer.  TBD
+    //We want the consumer thread to start first, it will block waiting for data from the SDR thread
+    consumerThread->start();
+    producerThread->start();
+    isThreadRunning = true;
+
+    return;
+}
+void SDRFile::Stop()
+{
+    if (isThreadRunning) {
+        producerThread->stop();
+        consumerThread->stop();
+        isThreadRunning = false;
+    }
+    return;
+}
 double SDRFile::GetStartupFrequency() {return 0;}
-int SDRFile::GetStartupMode() {return 0;}
+int SDRFile::GetStartupMode()
+{
+    return dmLSB;
+}
 double SDRFile::GetHighLimit() {return 0;}
 double SDRFile::GetLowLimit() {return 0;}
-double SDRFile::GetGain() {return 0;}
+//WIP: We really need an adjustable gain
+//For internally generated data, use .5
+double SDRFile::GetGain() {return 0.005;}
 QString SDRFile::GetDeviceName() {return "SDRFile";}
+int SDRFile::GetSampleRate()
+{
+    //WIP: This should return the sample rate from the wav file
+    if (wavFile != NULL)
+        //We've successfully opened and parsed chunks
+        return fmtSubChunk.sampleRate;
+    else
+        return 96000;
+}
+
+void SDRFile::StopProducerThread(){}
+void SDRFile::RunProducerThread()
+{
+    CPX sample;
+    int len;
+    if (wavFile != NULL) {
+        for (int i=0; i<framesPerBuffer; i++)
+        {
+            len = wavFile->read((char*)&data,sizeof(data));
+            if (len <= 0) {
+                sample.re = 0;
+                sample.im = 0;
+                inBuffer[i] = sample;
+            } else {
+                //Convert wav 16 bit (-32767 to +32767) int to sound card float32.  Values are -1 to +1
+                sample.re = data.left/32767.0;
+                sample.im = data.right/32767.0;
+                inBuffer[i] = sample;
+            }
+
+        }
+    }
+#if 0
+    //Testing output - need to run in thread eventually until Stop
+    for (int i=0; i<framesPerBuffer; i++){
+        sample = nco->NextSample();
+        inBuffer[i] = sample;
+    }
+#endif
+    receiver->ProcessBlock(inBuffer,outBuffer,framesPerBuffer);
+
+}
+void SDRFile::StopConsumerThread(){}
+void SDRFile::RunConsumerThread(){}
+
+
+bool SDRFile::OpenWavFile(char* fname)
+{
+    wavFile = new QFile(fname);
+    if (wavFile == NULL) {
+        return false;
+    }
+    bool res = wavFile->open(QFile::ReadOnly);
+    if (!res)
+        return false;
+    qint64 len = wavFile->read((char*)&riff,sizeof(riff));
+    if (len <=0 )
+        return false;
+    len = wavFile->read((char*)&fmtSubChunk,sizeof(fmtSubChunk));
+    if (len<0)
+        return false;
+    len = wavFile->read((char*)&dataSubChunk,sizeof(dataSubChunk));
+    if (len<0)
+        return false;
+#if 0
+    len = wavFile->read((char*)&data,sizeof(data));
+    if (len<0)
+        return false;
+    wavFile->close();
+#endif
+    return true;
+}
