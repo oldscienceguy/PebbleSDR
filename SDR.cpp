dc96dd64da339ffe1ef0e79f36636e2c8ce6152d
rlandsman <rlandsman@localhost>
2012-04-09 02:29:42 +0000

    Moved Sleeper class implementation to cpp file for good practice

--- SDR.cpp
+++ SDR.cpp
@@ -281,3 +281,16 @@ void SDRConsumerThread::run()
 		msleep(msSleep);
 	}
 }
+
+void Sleeper::usleep(unsigned long usecs)
+{
+    QThread::usleep(usecs);
+}
+void Sleeper::msleep(unsigned long msecs)
+{
+    QThread::msleep(msecs);
+}
+void Sleeper::sleep(unsigned long secs)
+{
+    QThread::sleep(secs);
+}
--- SDR.h
+++ SDR.h
@@ -139,7 +139,7 @@ private:
 class Sleeper : public QThread
 {
 public:
-    static void usleep(unsigned long usecs){QThread::usleep(usecs);}
-    static void msleep(unsigned long msecs){QThread::msleep(msecs);}
-    static void sleep(unsigned long secs){QThread::sleep(secs);}
+    static void usleep(unsigned long usecs);
+    static void msleep(unsigned long msecs);
+    static void sleep(unsigned long secs);
 };
