a7aac2064ced806510c555199e53829b22cc3260
rlandsman <rlandsman@localhost>
2012-05-21 03:51:22 +0000

    Completed producer/consumer.  Seems to work at 48k, but tuning sporadic.  I think there's something else needed to tune realtime

--- rtl2832.h
+++ rtl2832.h
@@ -1,6 +1,7 @@
 #ifndef RTL2832_H
 #define RTL2832_H
 
+#include "QSemaphore"
 #include "SDR.h"
 #include "rtl-sdr/rtl-sdr.h"
 
@@ -32,8 +33,23 @@ public:
 private:
     CPX *inBuffer;
     CPX *outBuffer;
-    unsigned char* producerBuffer;
+
+    int numDataBufs; //Producer/Consumer buffers
+    unsigned char **producerBuffer; //Array of buffers
     int producerBufferSize;
+    int nextProducerDataBuf;
+    int nextConsumerDataBuf;
+    /*
+      NumFreeBuffers starts at NUMDATABUFS and is decremented (acquire()) everytime the producer thread has new data.
+      If it ever gets to zero, it will block forever and program will hang until consumer thread catches up.
+      NumFreeBuffers is incremented (release()) in consumer thread when a buffer has been processed and can be reused.
+
+
+    */
+    QSemaphore *semNumFreeBuffers; //Init to NUMDATABUFS
+    QSemaphore *semNumFilledBuffers;
+
+
     int framesPerBuffer;
     quint16 rtlGain; //in 10ths of a db
     quint32 rtlFrequency;
@@ -42,43 +58,6 @@ private:
     quint16 rtlDecimate;
 
     rtlsdr_dev_t *dev;
-
-#if 0
-    bool RTL_I2CRead(quint8 addr, quint8 *buffer, int len);
-    int RTL_I2CWrite(quint8 i2c_addr, quint8 *buffer, int len);
-    bool RTL_ReadArray(quint8 block, quint16 addr, quint8 *array, quint8 len);
-    int RTL_WriteArray(quint8 block, quint16 addr, quint8 *array, quint8 len);
-    bool RTL_WriteReg(quint16 block, quint16 address, quint16 value, quint16 length);
-    bool DemodWriteReg(quint16 page, quint16 address, quint16 value, quint16 length);
-    bool InitTuner();
-    bool SetI2CRepeater(bool on);
-
-    bool RTL_Init();
-    bool E4000_Init();
-    bool E4000_I2CReadByte(quint8 addr, quint8 *byte);
-    bool E4000_I2CWriteByte(quint8 addr, quint8 byte);
-    bool E4000_I2CWriteArray(quint8 addr, quint8 *array, quint8 len);
-    bool E4000_ResetTuner();
-    bool E4000_TunerClock();
-    bool E4000_Qpeak();
-    bool E4000_DCoffloop();
-    bool E4000_GainControlinit();
-    bool E4000_SetBandwidthHz(unsigned long bandwidthHz);
-    bool E4000_IFFilter(int bandwidth);
-    bool E4000_SetRfFreqHz(unsigned long RfFreqHz);
-    bool E4000_GainManual();
-    bool E4000_GainFreq(int freq);
-    bool E4000_PLL(int ref_clk, int freq);
-    bool E4000_LNAFilter(int Freq);
-    bool E4000_FreqBand(int Freq);
-    bool E4000_DCoffLUT();
-    bool E4000_GainControlAuto();
-    bool E4000_Sensitivity(int Freq, int bandwidth);
-    bool E4000_Linearity(int Freq, int bandwidth);
-    bool E4000_Nominal(int Freq, int bandwidth);
-    quint32 crystalFreqHz;
-
-#endif
 
     QSettings *qSettings;
     USBUtil *usb;
