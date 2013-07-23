43fe8927e99294022efab169eab9028db17bd7f5
rlandsman <rlandsman@localhost>
2013-07-23 18:24:17 +0000

    Removed old ui/settings include that broke compile.  Old file happened to be left over in UI directory

--- Settings.h
+++ Settings.h
@@ -2,7 +2,6 @@
 //GPL license and attributions are in gpl.h and terms are included in this file by reference
 #include "gpl.h"
 #include <QSettings>
-#include "ui/ui_Settings.h"
 #include "sdr.h"
 #include "QFont"
 /*
