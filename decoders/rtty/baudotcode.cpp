da14f24c1b03bc4abb07f697db3e556b360c6145
rlandsman <rlandsman@localhost>
2013-08-31 02:13:18 +0000

    Adding new baudot code class

---
+++ decoders/rtty/baudotcode.cpp
@@ -0,0 +1,5 @@
+#include "baudotcode.h"
+
+BaudotCode::BaudotCode()
+{
+}
---
+++ decoders/rtty/baudotcode.h
@@ -0,0 +1,10 @@
+#ifndef BAUDOTCODE_H
+#define BAUDOTCODE_H
+
+class BaudotCode
+{
+public:
+    BaudotCode();
+};
+
+#endif // BAUDOTCODE_H
