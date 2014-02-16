2e1ea3acc65b6c8e9ae495bbe3c15fffcd6cab73
Richard Landsman <pebblesdr@gmail.com>
2014-02-15 20:56:45 -0800

    Bug: pa_Terminate wasn't being called when stream was stopped. Result was that device list was not refreshed when we switch devices

--- application/soundcard.cpp
+++ application/soundcard.cpp
@@ -127,6 +127,7 @@ int SoundCard::Stop()
 		error = Pa_StopStream(outStream);
 		error = Pa_CloseStream(outStream);
 	}
+	Pa_Terminate();
 	inStream = NULL;
 	outStream = NULL;
 	return 0;
@@ -207,6 +208,7 @@ QStringList SoundCard::DeviceList(bool typeInput)
 {
 	//This static method may get called before constructor
 	//So we have to make sure Pa_Initialize is called, make sure we have matching Pa_Terminate()
+	Pa_Terminate(); //Need to call this first to flush device list, in case it wasn't terminated previously
 	Pa_Initialize();
 	QStringList devList;
 	QString di;
