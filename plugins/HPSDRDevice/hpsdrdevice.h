4e1fd80710d167d4e1e4066792ee6d536ef4409d
Richard Landsman <pebblesdr@gmail.com>
2014-02-19 17:33:04 -0800

    Major update to all HPSDR constants and fixed broken tuning (always been broken) caused by incorrect Receiver # bit masks

--- plugins/HPSDRDevice/hpsdrdevice.h
+++ plugins/HPSDRDevice/hpsdrdevice.h
@@ -21,101 +21,123 @@ class HPSDRDevice : public QObject, public DeviceInterfaceBase
 	Q_INTERFACES(DeviceInterface)
 
 public:
-	static const int MAX_PACKET_SIZE = 64;
+	const int MAX_PACKET_SIZE = 64;
 
-	static const int IN_ENDPOINT2 = 0x82;
-	static const int OUT_ENDPOINT2 = 0x02; //Commands
-	static const int IN_ENDPOINT4 = 0x84; //Received bandscope data
-	static const int OUT_ENDPOINT4 = 0x04; //Received bandscope data
-	static const int IN_ENDPOINT6 = 0x86; //Received IQ data
-	static const int OUT_ENDPOINT6 = 0x06; //Received bandscope data
+	const int IN_ENDPOINT2 = 0x82;
+	const int OUT_ENDPOINT2 = 0x02; //Commands
+	const int IN_ENDPOINT4 = 0x84; //Received bandscope data
+	const int OUT_ENDPOINT4 = 0x04;
+	const int IN_ENDPOINT6 = 0x86; //Received IQ data
+	const int OUT_ENDPOINT6 = 0x06;
 
-	static const int OZY_FRAME_SIZE = 512; //Size of an Ozy frame
-	static const int NUMFRAMES = 24; //Number of frames to fetch at once to get 2048 samples
+	const int OZY_FRAME_SIZE = 512; //Size of an Ozy frame
 
 	//USB_TYPE_VENDOR | USB_ENDPOINT_IN | USB_RECIP_DEVICE
-	static const int VENDOR_REQ_TYPE_IN = 0xc0;
+	const int VENDOR_REQ_TYPE_IN = 0xc0;
 	//USB_TYPE_VENDOR | USB_ENDPOINT_OUT | USB_RECIP_DEVICE
-	static const int VENDOR_REQ_TYPE_OUT = 0x40;
+	const int VENDOR_REQ_TYPE_OUT = 0x40;
 
 
 	//Ozy Vendor req commands
-	static const int OZY_SET_LED = 0x01;
-	static const int OZY_LOAD_FPGA = 0x02;
+	const int OZY_SET_LED = 0x01;
+	const int OZY_LOAD_FPGA = 0x02;
 	//Index options for LOAD_FPGA command, Value=0
-	static const int FL_BEGIN = 0;
-	static const int FL_XFER = 1;
-	static const int FL_END = 2;
+	const int FL_BEGIN = 0;
+	const int FL_XFER = 1;
+	const int FL_END = 2;
 
-	static const int OZY_I2C_WRITE = 0x08;
-	static const int OZY_SDR1K_CTL = 0x0d;
+	const int OZY_I2C_WRITE = 0x08;
+	const int OZY_SDR1K_CTL = 0x0d;
 	//Value options for SDR1K_CTL, Index = 0
-	static const int SDR1K_CTL_READ_VERSION = 0x7;
+	const int SDR1K_CTL_READ_VERSION = 0x7;
 
-	static const int OZY_WRITE_RAM = 0xa0;
+	const int OZY_WRITE_RAM = 0xa0;
 
-	static const int OZY_TIMEOUT = 500; //Default timeout in ms
+	const int OZY_TIMEOUT = 500; //Default timeout in ms
 
-	static const int OZY_SYNC = 0x7f;
+	const int OZY_SYNC = 0x7f;
 
 	//Commands
-	static const int C0_MOX_INACTIVE = 0;
-	static const int C0_MOX_ACTIVE = 1;
-	static const int C0_FREQUENCY = 2; //If this is set, C1-C4 has frequency to set
+	const int C0_MOX_INACTIVE = 0x00;	//00000000
+	const int C0_MOX_ACTIVE = 0x01;		//00000001
 
-	static const int C1_SPEED_48KHZ = 0x00;
-	static const int C1_SPEED_96KHZ = 0x01;
-	static const int C1_SPEED_192KHZ = 0x02;
-	static const int C1_SPEED_384KHZ = 0x03;
+	const int C0_TX_FREQ  = 0x02;		//0000001x
+	const int C0_RX1_FREQ = 0x04;		//0000010x
+	const int C0_RX2_FREQ = 0x06;		//0000011x
+	const int C0_RX3_FREQ = 0x08;		//0000100x
+	const int C0_RX4_FREQ = 0x0a;		//0000101x
+	const int C0_RX5_FREQ = 0x0c;		//0000110x
+	const int C0_RX6_FREQ = 0x0e;		//0000111x
+	const int C0_RX7_FREQ = 0x10;		//0001000x
+
+	const int C0_EXTRA1 = 0x12;			//0001001x
+	const int C0_EXTRA2 = 0x14;			//0001010x
+
+	const int C1_SPEED_48KHZ = 0x00;	//00000000
+	const int C1_SPEED_96KHZ = 0x01;	//00000001
+	const int C1_SPEED_192KHZ = 0x02;	//00000010
+	const int C1_SPEED_384KHZ = 0x03;	//00000011
+
 	//What 10mhz clock do we use
-	static const int C1_10MHZ_ATLAS = 0x00; //Excalibur
-	static const int C1_10MHZ_PENELOPE = 0x04;
-	static const int C1_10MHZ_MERCURY = 0x08;
-	//What 122.8 hhz clock do we use
-	static const int C1_122MHZ_PENELOPE = 0x00;
-	static const int C1_122MHZ_MERCURY = 0x10;
-	//Atlas Bus Config
-	static const int C1_CONFIG_NONE = 0x00;
-	static const int C1_CONFIG_PENELOPE = 0x20;
-	static const int C1_CONFIG_MERCURY = 0x40;
-	static const int C1_CONFIG_BOTH = 0x60;
-	//Mic source
-	static const int C1_MIC_JANUS = 0x00;
-	static const int C1_MIC_PENELOPE = 0x80;
+	const int C1_10MHZ_ATLAS = 0x00;	//000000xx
+	const int C1_10MHZ_PENELOPE = 0x04;	//000001xx
+	const int C1_10MHZ_MERCURY = 0x08;	//000010xx
 
-	static const int C2_MODE_OTHERS = 0x00;
-	static const int C2_MODE_CLASSE = 0x01;
+	//What 122.8 hhz clock do we use
+	const int C1_122MHZ_PENELOPE = 0x00;//0000xxxx
+	const int C1_122MHZ_MERCURY = 0x10;	//0001xxxx
+
+	//Atlas Bus Config
+	const int C1_CONFIG_NONE = 0x00;	//000xxxxx
+	const int C1_CONFIG_PENELOPE = 0x20;//001xxxxx
+	const int C1_CONFIG_MERCURY = 0x40;	//010xxxxx
+	const int C1_CONFIG_BOTH = 0x60;	//011xxxxx
+
+	//Mic source
+	const int C1_MIC_JANUS = 0x00;		//0xxxxxxx
+	const int C1_MIC_PENELOPE = 0x80;	//1xxxxxxx
+
+	const int C2_MODE_OTHERS = 0x00;
+	const int C2_MODE_CLASSE = 0x01;
 
 	//Alex attenuator
-	static const int C3_ALEX_0DB = 0x00;
-	static const int C3_ALEX_10DB = 0x01;
-	static const int C3_ALEX_20DB = 0x02;
-	static const int C3_ALEX_30DB = 0x03;
+	const int C3_ALEX_0DB = 0x00;
+	const int C3_ALEX_10DB = 0x01;
+	const int C3_ALEX_20DB = 0x02;
+	const int C3_ALEX_30DB = 0x03;
 	//LT2208 ADC
-	static const int C3_LT2208_PREAMP_OFF = 0x00;
-	static const int C3_LT2208_PREAMP_ON = 0x04;
-	static const int C3_LT2208_DITHER_OFF = 0x0;
-	static const int C3_LT2208_DITHER_ON = 0x08;
-	static const int C3_LT2208_RANDOM_OFF = 0x00;
-	static const int C3_LT2208_RANDOM_ON = 0x10;
+	const int C3_LT2208_PREAMP_OFF = 0x00;
+	const int C3_LT2208_PREAMP_ON = 0x04;
+	const int C3_LT2208_DITHER_OFF = 0x0;
+	const int C3_LT2208_DITHER_ON = 0x08;
+	const int C3_LT2208_RANDOM_OFF = 0x00;
+	const int C3_LT2208_RANDOM_ON = 0x10;
 	//Alex antenna
-	static const int C3_ALEX_RXANT_NONE = 0x00;
-	static const int C3_ALEX_RXANT_RX1 = 0x20;
-	static const int C3_ALEX_RXANT_RX2 = 0x40;
-	static const int C3_ALEX_RXANT_XV = 0x60;
+	const int C3_ALEX_RXANT_NONE = 0x00;
+	const int C3_ALEX_RXANT_RX1 = 0x20;
+	const int C3_ALEX_RXANT_RX2 = 0x40;
+	const int C3_ALEX_RXANT_XV = 0x60;
 
-	static const int C3_ALEX_RXOUT_OFF = 0x00;
-	static const int C3_ALEX_RXOUT_ON = 0x80;
+	const int C3_ALEX_RXOUT_OFF = 0x00;
+	const int C3_ALEX_RXOUT_ON = 0x80;
 
-	static const int C4_ALEX_TXRELAY_TX1 = 0x00;
-	static const int C4_ALEX_TXRELAY_TX2 = 0x01;
-	static const int C4_ALEX_TXRELAY_TX3 = 0x02;
+	const int C4_ALEX_TXRELAY_TX1 = 0x00;
+	const int C4_ALEX_TXRELAY_TX2 = 0x01;
+	const int C4_ALEX_TXRELAY_TX3 = 0x02;
 
-	static const int C4_DUPLEX_OFF = 0x00;
-	static const int C4_DUPLEX_ON = 0x04;
+	const int C4_DUPLEX_OFF = 0x00;
+	const int C4_DUPLEX_ON = 0x04;
 
-	static const int C4_1RECEIVER = 0x00;
-	static const int C4_8RECEIVER = 0x38;
+	const int C4_1RECEIVER = 0x00; //000xxx
+	const int C4_2RECEIVER = 0x08; //001xxx
+	const int C4_3RECEIVER = 0x10; //010xxx
+	const int C4_4RECEIVER = 0x18; //011xxx
+	const int C4_5RECEIVER = 0x20; //100xxx
+	const int C4_6RECEIVER = 0x28; //101xxx
+	const int C4_7RECEIVER = 0x30; //110xxx
+	const int C4_8RECEIVER = 0x38; //111xxx
+
+	const int C4_CommonFreq = 0x80; //All receivers get same frequency
 
 	HPSDRDevice();
 	~HPSDRDevice();
@@ -134,11 +156,11 @@ public:
 	void SetupOptionUi(QWidget *parent);
 
 private slots:
-//Options dialog
-void preampChanged(bool b);
-void ditherChanged(bool b);
-void randomChanged(bool b);
-void optionsAccepted();
+	//Options dialog
+	void preampChanged(bool b);
+	void ditherChanged(bool b);
+	void randomChanged(bool b);
+	void optionsAccepted();
 
 
 private:
@@ -208,6 +230,8 @@ private:
 	int penelopeFW;
 
 	Ui::HPSDROptions *optionUi;
+
+	bool forceReload; //true will force firmware to be uploaded every time
 
 };
 #endif // HPSDRDEVICE_H
