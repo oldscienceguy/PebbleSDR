24e7ccb0000833e0eec98f870f3345e33b6c069b
Richard Landsman <pebblesdr@gmail.com>
2016-03-17 10:50:54 -0400

    Reversed re:im = cos:sin to sin:cos
    To sync with cleaned up FFT unfolding logic.  We were reversing the fft output, which is unnecessary, but that resulted in the mixer not working.  Reversing the mixer fixes this, but I don't understand why yet.  Flagged with Todo:

--- application/mixer.cpp
+++ application/mixer.cpp
@@ -19,10 +19,14 @@ Mixer::~Mixer(void)
 //This sets the frequency we want to mix, typically -48k to +48k
 void Mixer::SetFrequency(double f)
 {
-	frequency = f;
+	//3/17/16: For some reason, mixer is inverting
+	//1. Instead of re:im = cos:sin, we can switch to re:im = sin:cos
+	//2. We can reverse the freq
+	//Todo: Understand why this is so
+	frequency = -f;
 	//nco->setFrequency(f);
 
-	oscInc = TWOPI * frequency / sampleRate;
+	oscInc = (TWOPI / sampleRate) * frequency;
 	oscCos = cos(oscInc);
 	oscSin = sin(oscInc);
 	lastOsc.re = 1.0;
@@ -32,7 +36,7 @@ void Mixer::SetFrequency(double f)
 /*
 From Rick Muething, KN6KB DCC 2010 DSP course and texts noted in GPL.h 
 For j = 0 to Number of Samples -1
-    USB Out I (j) = NCOCos(j) * I(j) NCOSine(j) * Q(j)
+	USB Out I(j) = NCOCos(j) * I(j) - NCOSine(j) * Q(j)
 	LSB Out Q(j) = NCOSin(j) * I(j) + NCOCos(j) * Q(j)
 Next j
 */
@@ -50,20 +54,21 @@ CPX *Mixer::ProcessBlock(CPX *in)
 		//This is executed at the highest sample rate and every usec counts
 		//Pulled code from NCO for performance gain
 		osc.re = lastOsc.re * oscCos - lastOsc.im * oscSin;
-		osc.im = lastOsc.im * oscCos + lastOsc.re * oscSin;
+		osc.im = lastOsc.re * oscSin + lastOsc.im * oscCos;
 		//oscGn is used to account for variations in the amplitude output as time increases
 		//Since oscillator variables are not bounded as they are in alternate implementation (fmod(...))
 		oscGn = 1.95 - (lastOsc.re * lastOsc.re + lastOsc.im * lastOsc.im);
 		lastOsc.re = oscGn * osc.re;
 		lastOsc.im = oscGn * osc.im;
 #else
+		//Putting sin in re works with standard unfold, cos doesn't.  why?
 		osc.re = cos(oscTime);
 		osc.im = sin(oscTime);
 		oscTime += oscInc;
 		oscTime = fmod(oscTime, TWOPI);	//Keep bounded
 
 #endif
-
+		//out[i]= in[i] * osc;
 		out[i].convolution(osc, in[i]); //Inline code
 	}
 
