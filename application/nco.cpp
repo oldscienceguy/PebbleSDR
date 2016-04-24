e84e2a067b39e27c9e74331d325c2574d988a76d
Richard Landsman <pebblesdr@gmail.com>
2016-04-24 12:48:26 -0400

    Added genNoise and genSweep
    Removed and refactored from TestBench

--- application/nco.cpp
+++ application/nco.cpp
@@ -5,6 +5,7 @@
 NCO::NCO(quint32 _sampleRate, quint32 _bufferSize) :
 	ProcessStep(_sampleRate,_bufferSize)
 {
+	m_sweepInitialized = false;
 }
 
 NCO::~NCO(void)
@@ -14,7 +15,7 @@ void NCO::setFrequency(double f)
 {
 	//Eveeything related to frequency has to be set together, no interupts that could change freq 
 	//before step is set
-	mutex.lock();
+	m_mutex.lock();
 	//Saftey net, make sure freq is less than nyquist limit
 	int nyquist = (sampleRate / 2 ) -1;
 	if (f < - nyquist)
@@ -22,7 +23,7 @@ void NCO::setFrequency(double f)
 	else if (f > nyquist)
 		f = nyquist;
 
-	frequency = f;
+	m_frequency = f;
 	//Everytime freq changes, we restart synchronized NCO
 	//step is how much we have to increment each sample in sin(sample) to get a waveform that
 	//is perfectly synchronized to samplerate
@@ -32,13 +33,13 @@ void NCO::setFrequency(double f)
 	//ie angularIncrement will be neg for neg freq and pos for pos freq
 
 	//Only need to calculate these when freq changes, not in main generate loop
-	oscInc = TWOPI * frequency / sampleRate;
-	oscCos = cos(oscInc);
-	oscSin = sin(oscInc);
-	lastOsc.re = 1.0;
-	lastOsc.im = 0.0;
-	oscTime = 0.0;
-	mutex.unlock();
+	m_oscInc = TWOPI * m_frequency / sampleRate;
+	m_oscCos = cos(m_oscInc);
+	m_oscSin = sin(m_oscInc);
+	m_lastOsc.re = 1.0;
+	m_lastOsc.im = 0.0;
+	m_oscTime = 0.0;
+	m_mutex.unlock();
 }
 
 /*
@@ -59,14 +60,114 @@ At 48000 sps, this means we need 4800 samples for a complete cytle (sampleRate /
 So a 24khz signal will have 2 samples per cycle (48000/24000), which is the Nyquist limit
 -----------------
 */
-CPX * NCO::genSamples()
+void NCO::genSingle(CPX *_in, quint32 _numSamples, double _dbGain, bool _mix)
 {
-	//mutex.lock();
-
-	for (int i = 0; i < numSamples; i++) {
-		nextSample(out[i]);
+	for (int i = 0; i < _numSamples; i++) {
+		nextSample(_in[i]);
 	}
-	//next loop starts where we left off as if i=numSamples; i<numSamples*loop; ...
-	//mutex.unlock();
-	return out;
+
+}
+
+//Algorithm from http://dspguru.com/dsp/howtos/how-to-generate-white-gaussian-noise
+//Uses Box-Muller Method, optimized to Polar Method [Ros88] A First Course on Probability
+//Also http://www.dspguide.com/ch2/6.htm
+//https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform R Knop algorithm
+void NCO::genNoise(CPX *_in, quint32 _numSamples, double _dbGain, bool _mix)
+{
+	double u1;
+	double u2;
+	double s;
+	double rad;
+	double x;
+	double y;
+
+	//R Knop
+	for (int i=0; i<_numSamples; i++) {
+		do {
+			u1 = 1.0 - 2.0 * (double)rand()/(double)RAND_MAX;
+			u2 = 1.0 - 2.0 * (double)rand()/(double)RAND_MAX;
+			//Collapsing steps
+			s = u1 * u1 + u2 * u2;
+		} while (s >= 1.0 || s == 0.0);
+		// 0 < s < 1
+		rad = sqrt(-2.0 * log(s) / s);
+		x = _dbGain * u1 * rad;
+		y = _dbGain * u2 * rad;
+		if (_mix) {
+			_in[i].re += x;
+			_in[i].im += y;
+		} else {
+			_in[i].re = x;
+			_in[i].im = y;
+		}
+	}
+}
+
+//Resets sweep generator, must be called before genSweep
+void NCO::initSweep(double _sweepStartFreq, double _sweepStopFreq, double _sweepRate,
+	double _pulseWidth, double _pulsePeriod, SweepType _sweepType)
+{
+	m_sweepStartFreq = _sweepStartFreq;
+	m_sweepStopFreq = _sweepStopFreq;
+	m_sweepFreq = m_sweepStartFreq;
+	m_sweepRate = _sweepRate;
+	m_sweepAcc = 0.0;
+	m_sweepRateInc = m_sweepRate / sampleRate;
+	m_sweepFreqNorm = TWOPI / sampleRate;
+	m_sweepPulseWidth = _pulseWidth;
+	m_sweepPulsePeriod = _pulsePeriod;
+	m_sweepPulseTimer = 0;
+	m_sweepType = _sweepType;
+	m_sweepInitialized = true;
+}
+
+//Modifies _in
+void NCO::genSweep(CPX *_in, quint32 _numSamples, double _dbGain, bool _mix)
+{
+	if (!m_sweepInitialized)
+		return; //initSweep not called
+
+	double amp; //Used to toggle amplitude for pulses
+
+	for (int i=0; i<_numSamples; i++) {
+		amp = _dbGain;
+		if(m_sweepPulseWidth > 0.0) {
+			//if pulse width is >0 create pulse modulation
+			m_sweepPulseTimer += (1.0 / sampleRate);
+			if(m_sweepPulseTimer > m_sweepPulsePeriod)
+				m_sweepPulseTimer = 0.0;
+			if(m_sweepPulseTimer > m_sweepPulseWidth)
+				amp = 0.0; //Between pulses
+		}
+
+		//create complex sin/cos signal
+		if (_mix) {
+			//Add signal to incoming
+			_in[i].re += amp * cos(m_sweepAcc);
+			_in[i].im += amp * sin(m_sweepAcc);
+		} else {
+			//Replace incoming signal with generator
+			_in[i].re = amp * cos(m_sweepAcc);
+			_in[i].im = amp * sin(m_sweepAcc);
+		}
+		//inc phase accummulator with normalized freqeuency step
+		m_sweepAcc += ( m_sweepFreq * m_sweepFreqNorm );
+		m_sweepFreq += m_sweepRateInc;	//inc sweep frequency
+		if(m_sweepFreq >= m_sweepStopFreq) {	//reached end of sweep?
+			switch (m_sweepType) {
+				//3 choices: stop, start over, return in opposite direction
+				case SINGLE:
+					m_sweepRateInc = 0.0; //stop sweep when end is reached
+					break;
+				case REPEAT:
+					m_sweepFreq = m_sweepStartFreq;	//restart sweep when end is reached
+					break;
+				case REPEAT_REVERSE:
+					//Todo: Implement reverse sweep
+					break;
+			}
+
+		}
+	}
+	m_sweepAcc = (double)fmod((double)m_sweepAcc, TWOPI);	//keep radian counter bounded
 }
--- application/nco.h
+++ application/nco.h
@@ -20,8 +20,9 @@ public:
 	NCO(quint32 _sampleRate, quint32 _bufferSize);
 	~NCO(void);
 	void setFrequency(double f);
-	//Fills out buffer with samples
-	CPX * genSamples();
+
+	//Modifies _in with a single freq
+	void genSingle(CPX *_in, quint32 _numSamples, double _dbGain, bool _mix);
 
 	//Returns next sample for use in continuous loops
 	//Quadrature oscillator
@@ -29,31 +30,60 @@ public:
 		//More efficient code that doesn't recalc expensive sin/cos over and over
 		double oscGn;
 		//We could make osc complex and use CPX::convolution method with gain, same code
-		osc.re = lastOsc.re * oscCos - lastOsc.im * oscSin;
-		osc.im = lastOsc.im * oscCos + lastOsc.re * oscSin;
-		oscGn = 1.95 - (lastOsc.re * lastOsc.re + lastOsc.im * lastOsc.im);
-		lastOsc.re = oscGn * osc.re;
-		lastOsc.im = oscGn * osc.im;
+		osc.re = m_lastOsc.re * m_oscCos - m_lastOsc.im * m_oscSin;
+		osc.im = m_lastOsc.im * m_oscCos + m_lastOsc.re * m_oscSin;
+		oscGn = 1.95 - (m_lastOsc.re * m_lastOsc.re + m_lastOsc.im * m_lastOsc.im);
+		m_lastOsc.re = oscGn * osc.re;
+		m_lastOsc.im = oscGn * osc.im;
 	}
 
 	//Alternate implementation, much slower than quad oscillator because we recalc sin/cos every sample
 	inline void nextSampleAlt(CPX &osc) {
-		osc.re = cos(oscTime);
-		osc.im = sin(oscTime);
-		oscTime += oscInc;
-		oscTime = fmod(oscTime, TWOPI);	//Keep bounded
+		osc.re = cos(m_oscTime);
+		osc.im = sin(m_oscTime);
+		m_oscTime += m_oscInc;
+		m_oscTime = fmod(m_oscTime, TWOPI);	//Keep bounded
 
 	}
 
+	//Modifies _in
+	void genNoise(CPX* _in, quint32 _numSamples, double _dbGain, bool _mix);
+
+	enum SweepType {SINGLE, REPEAT, REPEAT_REVERSE};
+	//Must be called before genSweep
+	void initSweep(double _sweepStartFreq, double _sweepStopFreq, double _sweepRate,
+		double _pulseWidth, double _pulsePeriod, SweepType _sweepType = SINGLE);
+	//Modifies _in
+	void genSweep(CPX *_in, quint32 _numSamples, double _dbGain, bool _mix);
+
+	//Todo: genFM()
+	//Todo: genMorse()
+	//Other test modulations?
+
 private:
-	double oscInc;
-	double oscCos;
-	double oscSin;
-	double oscTime; //For alternate implementation
-	CPX lastOsc;
+	double m_oscInc;
+	double m_oscCos;
+	double m_oscSin;
+	double m_oscTime; //For alternate implementation
+	CPX m_lastOsc;
 
-	QMutex mutex;
+	QMutex m_mutex;
 
-	double frequency;
+	double m_frequency;
+
+	//For sweep
+	bool m_sweepInitialized;
+	double m_sweepStartFreq;
+	double m_sweepStopFreq;
+	double m_sweepFreq; //Frequency being generated
+	double m_sweepRate;
+	double m_sweepAcc;
+	double m_sweepRateInc;
+	double m_sweepFreqNorm;
+	SweepType m_sweepType;
+
+	double m_sweepPulseWidth;
+	double m_sweepPulsePeriod;
+	double m_sweepPulseTimer;
 
 };
