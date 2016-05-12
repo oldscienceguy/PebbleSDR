643b57c7aec569215274f3dd523de84e337e156a
Richard Landsman <pebblesdr@gmail.com>
2016-05-12 08:46:10 -0400

    Finally!  Less-like variables in qss(css) file
    Parsed in main.cpp

--- application/main.cpp
+++ application/main.cpp
@@ -12,7 +12,33 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     QFile qssFile( ":/pebble.qss" );  // assuming stylesheet is part of the resources
     qssFile.open( QFile::ReadOnly );
-    app.setStyleSheet( qssFile.readAll() );
+	QString qss = QTextStream(&qssFile).readAll();
+	//Find variables
+	int begin = 0;
+	int length = 0;
+	QString name;
+	QString value;
+	//@mycolor: #455090;
+	QRegularExpression varRegex = QRegularExpression("^(?<name>@.*):(?<value>.*);.*",QRegularExpression::MultilineOption);
+	QRegularExpressionMatch match;
+	//Loop until there are no more definitions
+	do {
+		match = varRegex.match(qss);
+		if (match.hasMatch()) {
+			//qDebug()<<match.captured();
+			name = match.captured("name");
+			value = match.captured("value");
+			//qDebug()<<name<<" "<<value;
+			begin = match.capturedStart();
+			length = match.capturedLength();
+			//Remove definition
+			qss.remove(match.capturedStart(), match.capturedLength());
+			//Replace all instances of var
+			qss.replace(name,value);
+		}
+	}while (match.hasMatch());
+
+	app.setStyleSheet(qss);
     qssFile.close();
 
     // Qt::FramelessWindowHint No title, size, nothing
