98f7453f785270b816587ab5b8dfd2ffc8de941e
Richard Landsman <pebblesdr@gmail.com>
2016-07-14 17:14:23 -0400

    New DoubleSlider class
    Compiles and displays, but doesn't have style or usage updates yet

--- application/doubleslider.cpp
+++ application/doubleslider.cpp
@@ -1,6 +1,166 @@
 #include "doubleslider.h"
 
-DoubleSlider::DoubleSlider()
+
+//Qt
+#include <QMouseEvent>
+#include "qmimedata.h"
+#include "qdrag.h"
+#include "qwidgetaction.h"
+#include "qapplication.h"
+#include "qpixmap.h"
+#include "qcursor.h"
+#include "qguiapplication.h"
+#include "qdir.h"
+#include <QProxyStyle>
+#include <QPainter>
+
+//See qstyle.h for style options
+class SliderProxy : public QProxyStyle
 {
+public:
+  int pixelMetric ( PixelMetric metric, const QStyleOption * option = 0, const QWidget * widget = 0 ) const
+  {
+	switch(metric) {
+	//case PM_SliderThickness  : return 25;
+	//case PM_SliderLength     : return 25;
+	default                  : return (QProxyStyle::pixelMetric(metric,option,widget));
+	}
+  }
+};
+
+DoubleSlider::DoubleSlider(QWidget *parent)
+  : QSlider(parent)
+{
+  //styling
+  setOrientation(Qt::Horizontal);
+  setAcceptDrops(true);
+  SliderProxy *aSliderProxy = new SliderProxy();
+
+#if 0
+  //If we want to use image as slider
+  QString path = QDir::fromNativeSeparators(ImagesPath("handle.png"));
+  setStyleSheet("QSlider::handle { image: url(" + path + "); }");
+#endif
+  setStyle(aSliderProxy); //All style requests go to our proxy so we pick up QSlider styles
+
+  //setting up the alternate handle
+  alt_handle = new DoubleSliderHandle(this);
+  addAction(new QWidgetAction(alt_handle));
+  alt_handle->move(this->pos().x() + this->width()- alt_handle->width(), this->pos().y() );
+
+}
+
+DoubleSliderHandle::DoubleSliderHandle(DoubleSlider *_parent)
+  : QLabel(_parent)
+{
+  parent = _parent;
+  filter = new DoubleSliderEventFilter(parent);
+
+  //styling
+  setAcceptDrops(true);
+#if 0
+  //hard coded path to image :/ sorry
+  QPixmap pix = QPixmap(ImagesPath("handle.png"));
+  pix =  pix.scaled(25, 25, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+#else
+  //We won't pick up pebble.qss styles here, so hard code equivalent for now
+  //Width is 3px + 1 px border
+  //Height is control height minus margin
+  quint32 h = this->height(); //typically 30px
+  QPixmap pix(5,h-12);
+  QRect rect = pix.rect();
+  QPainter painter(&pix);
+  painter.fillRect(rect, QColor(0x708090)); //From pebble.qss darkSteelGrey
+  painter.setPen(QColor(0x5c5c5c));
+  painter.drawRect(rect);
+
+#endif
+  setPixmap(pix);
+
+}
+
+int DoubleSlider::alt_value()
+{
+  return alt_handle->value();
+}
+
+void DoubleSlider::alt_setValue(int value)
+{
+  alt_handle->setValue(value);
+}
+
+void DoubleSlider::mouseReleaseEvent(QMouseEvent *mouseEvent)
+{
+  if (mouseEvent->button() == Qt::LeftButton)
+  {
+	alt_handle->show();
+	alt_handle->handleActivated = false;
+  }
+  mouseEvent->accept();
+}
+
+void DoubleSlider::alt_update()
+{
+  QPoint posCursor(QCursor::pos());
+  QPoint posParent(mapToParent(mapToGlobal(pos())));
+  QPoint point(alt_handle->mapToParent(alt_handle->mapFromGlobal(QCursor::pos())).x(),alt_handle->y());
+  int horBuffer = (alt_handle->width());
+  bool lessThanMax = mapToParent(point).x() < pos().x()+ width() - horBuffer;
+  bool greaterThanMin = mapToParent(point).x() > pos().x();
+  if(lessThanMax && greaterThanMin)
+	alt_handle->move(point);
+  emit alt_valueChanged(alt_value());
+}
+
+void DoubleSliderHandle::mousePressEvent(QMouseEvent *mouseEvent)
+{
+  qGuiApp->installEventFilter(filter);
+  parent->clearFocus();
+}
+
+bool DoubleSliderEventFilter::eventFilter(QObject* obj, QEvent* event)
+{
+  switch(event->type())
+  {
+  case QEvent::MouseButtonRelease:
+	qGuiApp->removeEventFilter(this);
+	return true;
+	break;
+  case QEvent::MouseMove:
+	grandParent->alt_update();
+	return true;
+	break;
+  default:
+	return QObject::eventFilter(obj, event);
+  }
+  return false;
+}
+
+void DoubleSliderHandle::setValue(double value)
+{
+  double width = parent->width(), position = pos().x();
+  double range = parent->maximum() - parent->minimum();
+  int location = (value - parent->minimum())/range;
+  location = location *width;
+  move(y(),location);
+}
+
+int DoubleSliderHandle::value()
+{
+  double width = parent->width(), position = pos().x();
+  double value = position/width;
+  double range = parent->maximum() - parent->minimum();
+  return parent->minimum() + (value * range);
+}
+void DoubleSlider::Reset()
+{
+  int horBuffer = (alt_handle->width());
+  QPoint myPos = mapToGlobal(pos());
+  QPoint point(myPos.x() + width() - horBuffer, myPos.y()- alt_handle->height());
+  point = alt_handle->mapFromParent(point);
+
+  alt_handle->move(point);
+  alt_handle->show();
+  alt_handle->raise();
 
 }
--- application/doubleslider.h
+++ application/doubleslider.h
@@ -1,12 +1,92 @@
 #ifndef DOUBLESLIDER_H
 #define DOUBLESLIDER_H
+//GPL license and attributions are in gpl.h and terms are included in this file by reference
+#include "gpl.h"
+
+//Derived from http://stackoverflow.com/questions/17361885/range-slider-in-qt-two-handles-in-a-qslider
+//Modified for general use in Pebble by Richard Landsman
 
 #include <QObject>
+#include <QSlider>
+#include <QLabel>
 
-class DoubleSlider : public QSlider
+class DoubleSliderHandle;
+
+class DoubleSlider: public QSlider
 {
+  Q_OBJECT
 public:
-	DoubleSlider();
+  /** Constructor */
+  DoubleSlider(QWidget *parent = 0);
+
+  /** Store the alternate handle for this slider*/
+  DoubleSliderHandle *alt_handle;
+
+  /** Overridden mouse release event */
+  void mouseReleaseEvent(QMouseEvent *event);
+
+  /** Returns the slider value for the alternate handle */
+  int alt_value();
+
+  /** Convenience function for setting the value of the alternate handle */
+  void alt_setValue(int value);
+
+  /** Resets the alternate handle to the right side of the slider */
+  void Reset();
+
+  /** Used to update the position of the alternate handle through the use of an event filter */
+  void alt_update();
+signals:
+  /** Constructor */
+  void alt_valueChanged(int);
 };
 
+class DoubleSliderEventFilter : public QObject
+{
+public:
+  /** Constructor */
+  DoubleSliderEventFilter(DoubleSlider *_grandParent)
+  {grandParent = _grandParent;}
+
+protected:
+  /*
+  *	overloaded functions for object that inherit from this class
+  */
+  bool eventFilter(QObject* obj, QEvent* event);
+
+private:
+  /** Store the SuperSlider that this event filter is used within. */
+  DoubleSlider *grandParent;
+};
+
+class DoubleSliderHandle: public QLabel
+{
+  Q_OBJECT
+public:
+  /** Constructor */
+  DoubleSliderHandle(DoubleSlider *parent = 0);
+
+  /** An overloaded mousePressevent so that we can start grabbing the cursor and using it's position for the value */
+  void mousePressEvent(QMouseEvent *event);
+
+  /** Returns the value of this handle with respect to the slider */
+  int value();
+
+  /** Maps mouse coordinates to slider values */
+  int mapValue();
+
+  /** Store the parent as a slider so that you don't have to keep casting it  */
+  DoubleSlider *parent;
+
+  /** Store a bool to determine if the alternate handle has been activated  */
+  bool handleActivated;
+
+private:
+  /** Store the filter for installation on the qguiapp */
+  DoubleSliderEventFilter *filter;
+
+  public slots:
+	/** Sets the value of the handle with respect to the slider */
+	void setValue(double value);
+};
 #endif // DOUBLESLIDER_H
